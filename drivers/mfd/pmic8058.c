9db8e052077417a5e97c3cc121a5b8e12561b930
Michael Bestas <mikeioannina@gmail.com>
2015-05-07 00:16:46 +0300

    mfd: pmic8058: pmic_keypad: Remove __dev*
    Change-Id: I81933e268f92422dea97d8d61066f7043bbda373

--- drivers/mfd/pmic8058.c
+++ drivers/mfd/pmic8058.c
@@ -405,7 +405,7 @@ static struct mfd_cell gpio_cell = {
 };
 
 #ifdef CONFIG_BOARD_SEMC_ZEUS
-static struct mfd_cell keypad_pmic_cell __devinitdata = {
+static struct mfd_cell keypad_pmic_cell = {
 	.name		= KP_NAME,
 	.id		= -1,
 };
