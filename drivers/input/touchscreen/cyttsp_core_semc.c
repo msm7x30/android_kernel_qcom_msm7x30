b41487eac893b616f5a9a1b7c69077871d8bd18f
Michael Bestas <mikeioannina@gmail.com>
2015-05-07 18:09:56 +0300

    input: touchscreen: cyttsp: Fix compilation for 3.4+ kernel
    Change-Id: I45ff163b3e73040236c04d8c31e4d94ffddf68a7

--- drivers/input/touchscreen/cyttsp_core_semc.c
+++ drivers/input/touchscreen/cyttsp_core_semc.c
@@ -1788,7 +1788,7 @@ static bool is_ttsp_fwwr_done(struct cyttsp *ts)
 			!(ts->bl_data.bl_error & ~CY_BL_RECEPTIVE);
 }
 
-static ssize_t firmware_write(struct kobject *kobj,
+static ssize_t firmware_write(struct file *ffile, struct kobject *kobj,
 				struct bin_attribute *bin_attr,
 				char *buf, loff_t pos, size_t size)
 {
@@ -1811,7 +1811,7 @@ static ssize_t firmware_write(struct kobject *kobj,
 	return -EAGAIN;
 }
 
-static ssize_t firmware_read(struct kobject *kobj,
+static ssize_t firmware_read(struct file *ffile, struct kobject *kobj,
 	struct bin_attribute *ba,
 	char *buf, loff_t pos, size_t size)
 {
