10f340b3366c4caef5ef8d6ddd6069fcdb351729
Rudolf Tammekivi <rtammekivi@gmail.com>
2015-07-30 13:52:52 +0300

    msm: restart_7k: Remove reboot notifier and clean code
    ARM restart handler already has special parameter to handle restart reason. Also
    disable FIQ interrupts during system power-off.
    
    Change-Id: I144f04d838d74e2834ae372bf48d04b77cc225eb

--- arch/arm/mach-msm/restart_7k.c
+++ arch/arm/mach-msm/restart_7k.c
@@ -11,22 +11,16 @@
  *
  */
 
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/reboot.h>
-#include <linux/pm.h>
-#include <asm/system_misc.h>
 #include <asm/idmap.h>
+#include <asm/system_misc.h>
+#include <linux/pm.h>
 #include <mach/proc_comm.h>
-
-static uint32_t restart_reason = 0x776655AA;
 
 static void msm_pm_power_off(void)
 {
 	/* Disable interrupts */
 	local_irq_disable();
+	local_fiq_disable();
 	msm_proc_comm(PCOM_POWER_DOWN, 0, 0);
 	for (;;)
 		;
@@ -34,7 +28,22 @@ static void msm_pm_power_off(void)
 
 static void msm_pm_restart(char str, const char *cmd)
 {
-	pr_debug("The reset reason is %x\n", restart_reason);
+	uint32_t restart_reason = 0x776655AA;
+
+	if (!strncmp(cmd, "bootloader", 10))
+		restart_reason = 0x77665500;
+	else if (!strncmp(cmd, "recovery", 8))
+		restart_reason = 0x77665502;
+	else if (!strncmp(cmd, "eraseflash", 10))
+		restart_reason = 0x776655EF;
+	else if (!strncmp(cmd, "oem-", 4)) {
+		unsigned long code;
+		int res;
+		res = kstrtoul(cmd + 4, 16, &code);
+		code &= 0xff;
+		restart_reason = 0x6f656d00 | code;
+	} else
+		restart_reason = 0x77665501;
 
 	/* Disable interrupts */
 	local_irq_disable();
@@ -49,51 +58,16 @@ static void msm_pm_restart(char str, const char *cmd)
 	 */
 	setup_mm_for_reboot();
 
+	pr_debug("The reset reason is %x\n", restart_reason);
 	msm_proc_comm(PCOM_RESET_CHIP, &restart_reason, 0);
-
 	for (;;)
 		;
 }
 
-static int msm_reboot_call
-	(struct notifier_block *this, unsigned long code, void *_cmd)
-{
-	if ((code == SYS_RESTART) && _cmd) {
-		char *cmd = _cmd;
-		if (!strncmp(cmd, "bootloader", 10)) {
-			restart_reason = 0x77665500;
-		} else if (!strncmp(cmd, "recovery", 8)) {
-			restart_reason = 0x77665502;
-		} else if (!strncmp(cmd, "eraseflash", 10)) {
-			restart_reason = 0x776655EF;
-		} else if (!strncmp(cmd, "oem-", 4)) {
-			unsigned long code;
-			int res;
-			res = kstrtoul(cmd + 4, 16, &code);
-			code &= 0xff;
-			restart_reason = 0x6f656d00 | code;
-		} else {
-			restart_reason = 0x77665501;
-		}
-	}
-	return NOTIFY_DONE;
-}
-
-static struct notifier_block msm_reboot_notifier = {
-	.notifier_call = msm_reboot_call,
-};
-
 static int __init msm_pm_restart_init(void)
 {
-	int ret;
-
 	pm_power_off = msm_pm_power_off;
 	arm_pm_restart = msm_pm_restart;
-
-	ret = register_reboot_notifier(&msm_reboot_notifier);
-	if (ret)
-		pr_err("Failed to register reboot notifier\n");
-
-	return ret;
+	return 0;
 }
 late_initcall(msm_pm_restart_init);
