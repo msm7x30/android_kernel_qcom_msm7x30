f1b539e3ec2ebe62e639f1a31976b9f58161c291
Rudolf Tammekivi <rtammekivi@gmail.com>
2015-08-02 21:45:22 +0300

    msm: rpc_server_handset: Add DT support
    Change-Id: Ie603e419526d932266cbf0cd77afa37213a731c4

--- arch/arm/mach-msm/rpc_server_handset.c
+++ arch/arm/mach-msm/rpc_server_handset.c
@@ -12,63 +12,57 @@
  * GNU General Public License for more details.
  */
 
-#include <linux/slab.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/platform_device.h>
 #include <linux/input.h>
+#include <linux/module.h>
+#include <linux/of.h>
 #include <linux/switch.h>
-
-#include <asm/mach-types.h>
-
 #include <mach/msm_rpcrouter.h>
-#include <mach/board.h>
 #include <mach/rpc_server_handset.h>
 
-#define DRIVER_NAME	"msm-handset"
+#define DRIVER_NAME		"msm-handset"
 
-#define HS_SERVER_PROG 0x30000062
-#define HS_SERVER_VERS 0x00010001
+#define HS_SERVER_PROG		0x30000062
+#define HS_SERVER_VERS		0x00010001
 
-#define HS_RPC_PROG 0x30000091
+#define HS_RPC_PROG		0x30000091
 
-#define HS_PROCESS_CMD_PROC 0x02
-#define HS_SUBSCRIBE_SRVC_PROC 0x03
-#define HS_REPORT_EVNT_PROC    0x05
+#define HS_PROCESS_CMD_PROC	0x02
+#define HS_SUBSCRIBE_SRVC_PROC	0x03
+#define HS_REPORT_EVNT_PROC	0x05
 #define HS_EVENT_CB_PROC	1
 #define HS_EVENT_DATA_VER	1
 
-#define RPC_KEYPAD_NULL_PROC 0
-#define RPC_KEYPAD_PASS_KEY_CODE_PROC 2
-#define RPC_KEYPAD_SET_PWR_KEY_STATE_PROC 3
+#define RPC_KEYPAD_NULL_PROC			0
+#define RPC_KEYPAD_PASS_KEY_CODE_PROC		2
+#define RPC_KEYPAD_SET_PWR_KEY_STATE_PROC	3
 
-#define HS_PWR_K		0x6F	/* Power key */
-#define HS_END_K		0x51	/* End key or Power key */
-#define HS_STEREO_HEADSET_K	0x82
-#define HS_HEADSET_SWITCH_K	0x84
-#define HS_HEADSET_SWITCH_2_K	0xF0
-#define HS_HEADSET_SWITCH_3_K	0xF1
-#define HS_HEADSET_HEADPHONE_K	0xF6
-#define HS_HEADSET_MICROPHONE_K 0xF7
-#define HS_REL_K		0xFF	/* key release */
+#define HS_PWR_K			0x6F	/* Power key */
+#define HS_END_K			0x51	/* End key or Power key */
+#define HS_STEREO_HEADSET_K		0x82
+#define HS_HEADSET_SWITCH_K		0x84
+#define HS_HEADSET_SWITCH_2_K		0xF0
+#define HS_HEADSET_SWITCH_3_K		0xF1
+#define HS_HEADSET_HEADPHONE_K		0xF6
+#define HS_HEADSET_MICROPHONE_K		0xF7
+#define HS_REL_K			0xFF	/* key release */
 
-#define SW_HEADPHONE_INSERT_W_MIC 1 /* HS with mic */
+#define SW_HEADPHONE_INSERT_W_MIC	1	/* HS with mic */
 
 #define KEY(hs_key, input_key) ((hs_key << 24) | input_key)
 
 enum hs_event {
-	HS_EVNT_EXT_PWR = 0,	/* External Power status        */
-	HS_EVNT_HSD,		/* Headset Detection            */
-	HS_EVNT_HSTD,		/* Headset Type Detection       */
-	HS_EVNT_HSSD,		/* Headset Switch Detection     */
+	HS_EVNT_EXT_PWR = 0,	/* External Power status */
+	HS_EVNT_HSD,		/* Headset Detection */
+	HS_EVNT_HSTD,		/* Headset Type Detection */
+	HS_EVNT_HSSD,		/* Headset Switch Detection */
 	HS_EVNT_KPD,
 	HS_EVNT_FLIP,		/* Flip / Clamshell status (open/close) */
 	HS_EVNT_CHARGER,	/* Battery is being charged or not */
 	HS_EVNT_ENV,		/* Events from runtime environment like DEM */
 	HS_EVNT_REM,		/* Events received from HS counterpart on a
-				remote processor*/
+				   remote processor */
 	HS_EVNT_DIAG,		/* Diag Events  */
-	HS_EVNT_LAST,		 /* Should always be the last event type */
+	HS_EVNT_LAST,		/* Should always be the last event type */
 	HS_EVNT_MAX		/* Force enum to be an 32-bit number */
 };
 
@@ -79,52 +73,56 @@ enum hs_src_state {
 };
 
 struct hs_event_data {
-	uint32_t	ver;		/* Version number */
-	enum hs_event	event_type;     /* Event Type	*/
-	enum hs_event	enum_disc;     /* discriminator */
-	uint32_t	data_length;	/* length of the next field */
-	enum hs_src_state	data;    /* Pointer to data */
-	uint32_t	data_size;	/* Elements to be processed in data */
+	uint32_t		ver;		/* Version number */
+	enum hs_event		event_type;	/* Event Type	*/
+	enum hs_event		enum_disc;	/* discriminator */
+	uint32_t		data_length;	/* length of the next field */
+	enum hs_src_state	data;		/* Pointer to data */
+	uint32_t		data_size;	/* Elements to be processed in
+						   data */
 };
 
 enum hs_return_value {
-	HS_EKPDLOCKED     = -2,	/* Operation failed because keypad is locked */
-	HS_ENOTSUPPORTED  = -1,	/* Functionality not supported */
-	HS_FALSE          =  0, /* Inquired condition is not true */
-	HS_FAILURE        =  0, /* Requested operation was not successful */
-	HS_TRUE           =  1, /* Inquired condition is true */
-	HS_SUCCESS        =  1, /* Requested operation was successful */
-	HS_MAX_RETURN     =  0x7FFFFFFF/* Force enum to be a 32 bit number */
+	HS_EKPDLOCKED		= -2, /* Operation failed because keypad is
+					 locked */
+	HS_ENOTSUPPORTED	= -1, /* Functionality not supported */
+	HS_FALSE		=  0, /* Inquired condition is not true */
+	HS_FAILURE		=  0, /* Requested operation was not
+					 successful */
+	HS_TRUE			=  1, /* Inquired condition is true */
+	HS_SUCCESS		=  1, /* Requested operation was successful */
+	HS_MAX_RETURN		=  0x7FFFFFFF /* Force enum to be a 32 bit
+						 number */
 };
 
 struct hs_key_data {
-	uint32_t ver;        /* Version number to track sturcture changes */
-	uint32_t code;       /* which key? */
-	uint32_t parm;       /* key status. Up/down or pressed/released */
+	uint32_t ver;		/* Version number to track sturcture changes */
+	uint32_t code;		/* which key? */
+	uint32_t parm;		/* key status. Up/down or pressed/released */
 };
 
 enum hs_subs_srvc {
-	HS_SUBS_SEND_CMD = 0, /* Subscribe to send commands to HS */
-	HS_SUBS_RCV_EVNT,     /* Subscribe to receive Events from HS */
+	HS_SUBS_SEND_CMD = 0,	/* Subscribe to send commands to HS */
+	HS_SUBS_RCV_EVNT, 	/* Subscribe to receive Events from HS */
 	HS_SUBS_SRVC_MAX
 };
 
 enum hs_subs_req {
-	HS_SUBS_REGISTER,    /* Subscribe   */
-	HS_SUBS_CANCEL,      /* Unsubscribe */
+	HS_SUBS_REGISTER,	/* Subscribe */
+	HS_SUBS_CANCEL,		/* Unsubscribe */
 	HS_SUB_STATUS_MAX
 };
 
 enum hs_event_class {
-	HS_EVNT_CLASS_ALL = 0, /* All HS events */
-	HS_EVNT_CLASS_LAST,    /* Should always be the last class type   */
+	HS_EVNT_CLASS_ALL = 0,	/* All HS events */
+	HS_EVNT_CLASS_LAST,	/* Should always be the last class type */
 	HS_EVNT_CLASS_MAX
 };
 
 enum hs_cmd_class {
-	HS_CMD_CLASS_LCD = 0, /* Send LCD related commands              */
-	HS_CMD_CLASS_KPD,     /* Send KPD related commands              */
-	HS_CMD_CLASS_LAST,    /* Should always be the last class type   */
+	HS_CMD_CLASS_LCD = 0,	/* Send LCD related commands */
+	HS_CMD_CLASS_KPD,	/* Send KPD related commands */
+	HS_CMD_CLASS_LAST,	/* Should always be the last class type */
 	HS_CMD_CLASS_MAX
 };
 
@@ -137,12 +135,12 @@ union hs_subs_class {
 };
 
 struct hs_subs {
-	uint32_t                ver;
-	enum hs_subs_srvc	srvc;  /* commands or events */
-	enum hs_subs_req	req;   /* subscribe or unsubscribe  */
+	uint32_t		ver;
+	enum hs_subs_srvc	srvc;	/* commands or events */
+	enum hs_subs_req	req;	/* subscribe or unsubscribe  */
 	uint32_t		host_os;
-	enum hs_subs_req	disc;  /* discriminator    */
-	union hs_subs_class      id;
+	enum hs_subs_req	disc;	/* discriminator    */
+	union hs_subs_class	id;
 };
 
 struct hs_event_cb_recv {
@@ -151,33 +149,39 @@ struct hs_event_cb_recv {
 	struct hs_key_data key;
 };
 enum hs_ext_cmd_type {
-	HS_EXT_CMD_KPD_SEND_KEY = 0, /* Send Key */
-	HS_EXT_CMD_KPD_BKLT_CTRL, /* Keypad backlight intensity	*/
-	HS_EXT_CMD_LCD_BKLT_CTRL, /* LCD Backlight intensity */
-	HS_EXT_CMD_DIAG_KEYMAP, /* Emulating a Diag key sequence */
-	HS_EXT_CMD_DIAG_LOCK, /* Device Lock/Unlock */
-	HS_EXT_CMD_GET_EVNT_STATUS, /* Get the status for one of the drivers */
-	HS_EXT_CMD_KPD_GET_KEYS_STATUS,/* Get a list of keys status */
-	HS_EXT_CMD_KPD_SET_PWR_KEY_RST_THOLD, /* PWR Key HW Reset duration */
-	HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD, /* Set pwr key threshold duration */
-	HS_EXT_CMD_LAST, /* Should always be the last command type */
-	HS_EXT_CMD_MAX = 0x7FFFFFFF /* Force enum to be an 32-bit number */
+	HS_EXT_CMD_KPD_SEND_KEY = 0,		/* Send Key */
+	HS_EXT_CMD_KPD_BKLT_CTRL,		/* Keypad backlight intensity */
+	HS_EXT_CMD_LCD_BKLT_CTRL,		/* LCD Backlight intensity */
+	HS_EXT_CMD_DIAG_KEYMAP,			/* Emulating a Diag key
+						   sequence */
+	HS_EXT_CMD_DIAG_LOCK,			/* Device Lock/Unlock */
+	HS_EXT_CMD_GET_EVNT_STATUS,		/* Get the status for one of the
+						   drivers */
+	HS_EXT_CMD_KPD_GET_KEYS_STATUS,		/* Get a list of keys status */
+	HS_EXT_CMD_KPD_SET_PWR_KEY_RST_THOLD,	/* PWR Key HW Reset duration */
+	HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD,	/* Set pwr key threshold
+						   duration */
+	HS_EXT_CMD_LAST,			/* Should always be the last
+						   command type */
+	HS_EXT_CMD_MAX = 0x7FFFFFFF		/* Force enum to be an 32-bit
+						   number */
 };
 
 struct hs_cmd_data_type {
-	uint32_t hs_cmd_data_type_ptr; /* hs_cmd_data_type ptr length */
-	uint32_t ver; /* version */
-	enum hs_ext_cmd_type id; /* command id */
-	uint32_t handle; /* handle returned from subscribe proc */
-	enum hs_ext_cmd_type disc_id1; /* discriminator id */
-	uint32_t input_ptr; /* input ptr length */
-	uint32_t input_val; /* command specific data */
-	uint32_t input_len; /* length of command input */
-	enum hs_ext_cmd_type disc_id2; /* discriminator id */
-	uint32_t output_len; /* length of output data */
-	uint32_t delayed; /* execution context for modem
-				true - caller context
-				false - hs task context*/
+	uint32_t hs_cmd_data_type_ptr;	/* hs_cmd_data_type ptr length */
+	uint32_t ver;			/* version */
+	enum hs_ext_cmd_type id;	/* command id */
+	uint32_t handle;		/* handle returned from subscribe
+					   proc */
+	enum hs_ext_cmd_type disc_id1;	/* discriminator id */
+	uint32_t input_ptr;		/* input ptr length */
+	uint32_t input_val;		/* command specific data */
+	uint32_t input_len;		/* length of command input */
+	enum hs_ext_cmd_type disc_id2;	/* discriminator id */
+	uint32_t output_len;		/* length of output data */
+	uint32_t delayed;		/* execution context for modem
+					    true - caller context
+					    false - hs task context */
 };
 
 static const uint32_t hs_key_map[] = {
@@ -196,12 +200,14 @@ enum {
 	NO_DEVICE	= 0,
 	MSM_HEADSET	= 1,
 };
+
 /* Add newer versions at the top of array */
 static const unsigned int rpc_vers[] = {
 	0x00030001,
 	0x00020001,
 	0x00010001,
 };
+
 /* hs subscription request parameters */
 struct hs_subs_rpc_req {
 	uint32_t hs_subs_ptr;
@@ -287,10 +293,9 @@ static void report_hs_key(uint32_t key_code, uint32_t key_parm)
 		break;
 	case SW_HEADPHONE_INSERT_W_MIC:
 		hs->mic_on = hs->hs_on = (key_code != HS_REL_K) ? 1 : 0;
-		input_report_switch(hs->ipdev, SW_HEADPHONE_INSERT,
-							hs->hs_on);
+		input_report_switch(hs->ipdev, SW_HEADPHONE_INSERT, hs->hs_on);
 		input_report_switch(hs->ipdev, SW_MICROPHONE_INSERT,
-							hs->mic_on);
+				    hs->mic_on);
 		update_state();
 		break;
 
@@ -305,15 +310,15 @@ static void report_hs_key(uint32_t key_code, uint32_t key_parm)
 		update_state();
 		break;
 	case -1:
-		printk(KERN_ERR "%s: No mapping for remote handset event %d\n",
-				 __func__, temp_key_code);
+		pr_err("%s: No mapping for remote handset event %d\n",
+		       __func__, temp_key_code);
 		return;
 	}
 	input_sync(hs->ipdev);
 }
 
 static int handle_hs_rpc_call(struct msm_rpc_server *server,
-			   struct rpc_request_hdr *req, unsigned len)
+			      struct rpc_request_hdr *req, unsigned len)
 {
 	struct rpc_keypad_pass_key_code_args {
 		uint32_t key_code;
@@ -337,10 +342,11 @@ static int handle_hs_rpc_call(struct msm_rpc_server *server,
 	}
 
 	case RPC_KEYPAD_SET_PWR_KEY_STATE_PROC:
-		/* This RPC function must be available for the ARM9
-		 * to function properly.  This function is redundant
-		 * when RPC_KEYPAD_PASS_KEY_CODE_PROC is handled. So
-		 * input_report_key is not needed.
+		/*
+		 * This RPC function must be available for the ARM9 to function
+		 * properly. This function is redundant when
+		 * RPC_KEYPAD_PASS_KEY_CODE_PROC is handled. So input_report_key
+		 * is not needed.
 		 */
 		return 0;
 	default:
@@ -385,7 +391,7 @@ static void process_hs_rpc_request(uint32_t proc, void *data)
 }
 
 static int hs_rpc_report_event_arg(struct msm_rpc_client *client,
-					void *buffer, void *data)
+				   void *buffer, void *data)
 {
 	struct hs_event_rpc_req {
 		uint32_t hs_event_data_ptr;
@@ -406,7 +412,7 @@ static int hs_rpc_report_event_arg(struct msm_rpc_client *client,
 }
 
 static int hs_rpc_report_event_res(struct msm_rpc_client *client,
-					void *buffer, void *data)
+				   void *buffer, void *data)
 {
 	enum hs_return_value result;
 
@@ -439,28 +445,27 @@ void report_headset_status(bool connected)
 EXPORT_SYMBOL(report_headset_status);
 
 static int hs_rpc_pwr_cmd_arg(struct msm_rpc_client *client,
-				    void *buffer, void *data)
+			      void *buffer, void *data)
 {
 	struct hs_cmd_data_type *hs_pwr_cmd = buffer;
 
 	hs_pwr_cmd->hs_cmd_data_type_ptr = cpu_to_be32(0x01);
-
-	hs_pwr_cmd->ver = cpu_to_be32(0x03);
-	hs_pwr_cmd->id = cpu_to_be32(HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD);
-	hs_pwr_cmd->handle = cpu_to_be32(hs_subs_req->hs_handle_data);
-	hs_pwr_cmd->disc_id1 = cpu_to_be32(HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD);
-	hs_pwr_cmd->input_ptr = cpu_to_be32(0x01);
-	hs_pwr_cmd->input_val = cpu_to_be32(hs->pdata->pwr_key_delay_ms);
-	hs_pwr_cmd->input_len = cpu_to_be32(0x01);
-	hs_pwr_cmd->disc_id2 = cpu_to_be32(HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD);
-	hs_pwr_cmd->output_len = cpu_to_be32(0x00);
-	hs_pwr_cmd->delayed = cpu_to_be32(0x00);
+	hs_pwr_cmd->ver		= cpu_to_be32(0x03);
+	hs_pwr_cmd->id		= cpu_to_be32(HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD);
+	hs_pwr_cmd->handle	= cpu_to_be32(hs_subs_req->hs_handle_data);
+	hs_pwr_cmd->disc_id1	= cpu_to_be32(HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD);
+	hs_pwr_cmd->input_ptr	= cpu_to_be32(0x01);
+	hs_pwr_cmd->input_val	= cpu_to_be32(hs->pdata->pwr_key_delay_ms);
+	hs_pwr_cmd->input_len	= cpu_to_be32(0x01);
+	hs_pwr_cmd->disc_id2	= cpu_to_be32(HS_EXT_CMD_KPD_SET_PWR_KEY_THOLD);
+	hs_pwr_cmd->output_len	= cpu_to_be32(0x00);
+	hs_pwr_cmd->delayed	= cpu_to_be32(0x00);
 
 	return sizeof(*hs_pwr_cmd);
 }
 
 static int hs_rpc_pwr_cmd_res(struct msm_rpc_client *client,
-				    void *buffer, void *data)
+			      void *buffer, void *data)
 {
 	uint32_t result;
 
@@ -515,11 +520,9 @@ static int hs_cb_func(struct msm_rpc_client *client, void *buffer, int in_size)
 	hdr->vers = be32_to_cpu(hdr->vers);
 	hdr->procedure = be32_to_cpu(hdr->procedure);
 
-	process_hs_rpc_request(hdr->procedure,
-			    (void *) (hdr + 1));
+	process_hs_rpc_request(hdr->procedure, (void *) (hdr + 1));
 
-	msm_rpc_start_accepted_reply(client, hdr->xid,
-				     RPC_ACCEPTSTAT_SUCCESS);
+	msm_rpc_start_accepted_reply(client, hdr->xid, RPC_ACCEPTSTAT_SUCCESS);
 	rc = msm_rpc_send_accepted_reply(client, 0);
 	if (rc) {
 		pr_err("%s: sending reply failed: %d\n", __func__, rc);
@@ -536,8 +539,9 @@ static int hs_rpc_cb_init(void)
 	num_vers = ARRAY_SIZE(rpc_vers);
 
 	for (i = 0; i < num_vers; i++) {
-		rpc_client = msm_rpc_register_client("hs",
-			HS_RPC_PROG, rpc_vers[i], 0, hs_cb_func);
+		rpc_client = msm_rpc_register_client("hs", HS_RPC_PROG,
+						     rpc_vers[i], 0,
+						     hs_cb_func);
 
 		if (IS_ERR(rpc_client))
 			pr_debug("%s: RPC Client version %d failed, fallback\n",
@@ -548,7 +552,7 @@ static int hs_rpc_cb_init(void)
 
 	if (IS_ERR(rpc_client)) {
 		pr_err("%s: Incompatible RPC version error %ld\n",
-			 __func__, PTR_ERR(rpc_client));
+		       __func__, PTR_ERR(rpc_client));
 		return PTR_ERR(rpc_client);
 	}
 
@@ -557,16 +561,16 @@ static int hs_rpc_cb_init(void)
 				hs_rpc_register_subs_res, NULL, -1);
 	if (rc) {
 		pr_err("%s: RPC client request failed for subscribe services\n",
-						__func__);
+		       __func__);
 		goto err_client_req;
 	}
 
 	rc = msm_rpc_client_req(rpc_client, HS_PROCESS_CMD_PROC,
-			hs_rpc_pwr_cmd_arg, NULL,
-			hs_rpc_pwr_cmd_res, NULL, -1);
+				hs_rpc_pwr_cmd_arg, NULL,
+				hs_rpc_pwr_cmd_res, NULL, -1);
 	if (rc)
-		pr_err("%s: RPC client request failed for pwr key"
-			" delay cmd, using normal mode\n", __func__);
+		pr_err("%s: RPC client request failed for pwr key "
+		       "delay cmd, using normal mode\n", __func__);
 	return 0;
 err_client_req:
 	msm_rpc_unregister_client(rpc_client);
@@ -580,7 +584,7 @@ static int hs_rpc_init(void)
 	rc = hs_rpc_cb_init();
 	if (rc) {
 		pr_err("%s: failed to initialize rpc client, try server...\n",
-						__func__);
+		       __func__);
 
 		rc = msm_rpc_create_server(&hs_rpc_server);
 		if (rc) {
@@ -613,38 +617,73 @@ static int hs_probe(struct platform_device *pdev)
 {
 	int rc = 0;
 	struct input_dev *ipdev;
+	struct device *dev = &pdev->dev;
+	struct msm_handset_platform_data *pdata = dev_get_platdata(dev);
+	struct device_node *node = dev->of_node;
 
-	hs = kzalloc(sizeof(struct msm_handset), GFP_KERNEL);
+	if (!pdata && !node) {
+		dev_err(dev, "no platform/device tree data supplied\n");
+		return -EINVAL;
+	}
+
+	hs = devm_kzalloc(dev, sizeof(struct msm_handset), GFP_KERNEL);
 	if (!hs)
 		return -ENOMEM;
 
-	hs->sdev.name	= "h2w";
+	if (node) {
+		pdata = devm_kzalloc(dev,
+				     sizeof(struct msm_handset_platform_data),
+				     GFP_KERNEL);
+		if (!pdata) {
+			dev_err(dev, "failed to allocate memory\n");
+			return -ENOMEM;
+		}
+
+		rc = of_property_read_string(node, "hs-name",
+			&pdata->hs_name);
+		if (rc) {
+			dev_warn(dev, "failed to read 'hs-name' rc=%d\n",
+				 rc);
+			rc = 0; /* fall through */
+		}
+
+		rc = of_property_read_u32(node, "pwr-key-delay-ms",
+			&pdata->pwr_key_delay_ms);
+		if (rc) {
+			dev_warn(dev,
+				 "failed to read 'pwr-key-delay-ms' rc=%d\n",
+				 rc);
+			rc = 0; /* fall through */
+		}
+	}
+
+	hs->pdata = pdata;
+
+	hs->sdev.name = "h2w";
 	hs->sdev.print_name = msm_headset_print_name;
 
 	rc = switch_dev_register(&hs->sdev);
 	if (rc)
-		goto err_switch_dev_register;
+		return rc;
 
 	ipdev = input_allocate_device();
 	if (!ipdev) {
 		rc = -ENOMEM;
 		goto err_alloc_input_dev;
 	}
-	input_set_drvdata(ipdev, hs);
 
 	hs->ipdev = ipdev;
 
-	if (pdev->dev.platform_data)
-		hs->pdata = pdev->dev.platform_data;
+	input_set_drvdata(ipdev, hs);
 
 	if (hs->pdata->hs_name)
 		ipdev->name = hs->pdata->hs_name;
 	else
-		ipdev->name	= DRIVER_NAME;
+		ipdev->name = DRIVER_NAME;
 
-	ipdev->id.vendor	= 0x0001;
-	ipdev->id.product	= 1;
-	ipdev->id.version	= 1;
+	ipdev->id.vendor = 0x0001;
+	ipdev->id.product = 1;
+	ipdev->id.version = 1;
 
 	input_set_capability(ipdev, EV_KEY, KEY_MEDIA);
 	input_set_capability(ipdev, EV_KEY, KEY_VOLUMEUP);
@@ -656,18 +695,18 @@ static int hs_probe(struct platform_device *pdev)
 
 	rc = input_register_device(ipdev);
 	if (rc) {
-		dev_err(&ipdev->dev,
-				"hs_probe: input_register_device rc=%d\n", rc);
+		dev_err(dev, "failed to register input device rc=%d\n",
+			rc);
 		goto err_reg_input_dev;
 	}
 
-	platform_set_drvdata(pdev, hs);
-
 	rc = hs_rpc_init();
 	if (rc) {
-		dev_err(&ipdev->dev, "rpc init failure\n");
+		dev_err(dev, "rpc init failure rc=%d\n", rc);
 		goto err_hs_rpc_init;
 	}
+
+	platform_set_drvdata(pdev, hs);
 
 	return 0;
 
@@ -678,8 +717,6 @@ err_reg_input_dev:
 	input_free_device(ipdev);
 err_alloc_input_dev:
 	switch_dev_unregister(&hs->sdev);
-err_switch_dev_register:
-	kfree(hs);
 	return rc;
 }
 
@@ -687,19 +724,29 @@ static int hs_remove(struct platform_device *pdev)
 {
 	struct msm_handset *hs = platform_get_drvdata(pdev);
 
-	input_unregister_device(hs->ipdev);
-	switch_dev_unregister(&hs->sdev);
-	kfree(hs);
 	hs_rpc_deinit();
+	input_unregister_device(hs->ipdev);
+	input_free_device(hs->ipdev);
+	switch_dev_unregister(&hs->sdev);
+
 	return 0;
 }
 
+#ifdef CONFIG_OF
+static struct of_device_id hs_of_match_table[] = {
+	{ .compatible = "qcom,msm-handset", },
+	{ /* end of table */ }
+};
+MODULE_DEVICE_TABLE(of, hs_of_match_table);
+#endif
+
 static struct platform_driver hs_driver = {
-	.probe		= hs_probe,
-	.remove		= hs_remove,
-	.driver		= {
-		.name	= DRIVER_NAME,
-		.owner	= THIS_MODULE,
+	.probe	= hs_probe,
+	.remove	= hs_remove,
+	.driver	= {
+		.name		= DRIVER_NAME,
+		.owner		= THIS_MODULE,
+		.of_match_table	= of_match_ptr(hs_of_match_table),
 	},
 };
 
