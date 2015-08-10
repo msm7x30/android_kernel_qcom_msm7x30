ae31e505ce0a955c8ce06739dd97779aa64ec7ec
Rudolf Tammekivi <rtammekivi@gmail.com>
2015-08-10 17:23:18 +0300

    msm: proccomm-regulator: Convert to DT
    Other changes include:
     * One device per regulator
     * Cleanup headers
     * Simplify code and follow common pattern
     * Use proper names for variables
    
    Change-Id: If69e920d6b5c636697d591e158418afba80935fc

--- arch/arm/mach-msm/proccomm-regulator.c
+++ arch/arm/mach-msm/proccomm-regulator.c
@@ -11,21 +11,26 @@
  * GNU General Public License for more details.
  */
 
-#define pr_fmt(fmt) "%s: " fmt, __func__
-
 #include <linux/module.h>
-#include <linux/slab.h>
-#include <linux/err.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
+#include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/driver.h>
 #include <linux/regulator/machine.h>
+#include <linux/regulator/of_regulator.h>
 #include <mach/proc_comm.h>
-#include "proccomm-regulator.h"
 
-#define MV_TO_UV(mv) ((mv)*1000)
-#define UV_TO_MV(uv) (((uv)+999)/1000)
+#define PROCCOMM_REGULATOR_DEV_NAME "proccomm-regulator"
+
+#define MV_TO_UV(mv)		((mv)*1000)
+#define UV_TO_MV(uv)		((uv)/1000)
+
+#if defined(CONFIG_MSM_VREG_SWITCH_INVERTED)
+#define VREG_SWITCH_ENABLE	0
+#define VREG_SWITCH_DISABLE	1
+#else
+#define VREG_SWITCH_ENABLE	1
+#define VREG_SWITCH_DISABLE	0
+#endif
 
 /*
  * Wrappers for the msm_proc_comm() calls.
@@ -38,9 +43,9 @@ static int _pcom_err_to_linux_errno(unsigned error)
 {
 	if (!error)		/* 0 == no error */
 		return 0;
-	else if (error & 0x1F)  /* bits 0..4 => parameter 1..5 out of range */
+	else if (error & 0x1F)	/* bits 0..4 => parameter 1..5 out of range */
 		return -EDOM;
-	else if (error & 0x100) /* bit 8 => feature not supported */
+	else if (error & 0x100)	/* bit 8 => feature not supported */
 		return -ENOSYS;
 	else			/* anything else non-zero: unknown error */
 		return -EINVAL;
@@ -58,14 +63,13 @@ static int _vreg_switch(int vreg_id, bool enable)
 /* vreg_set_level: (vreg ID, mV) => (return code, <null>) */
 static int _vreg_set_level(int vreg_id, int level_mV)
 {
+	int ret;
 	unsigned _id		= (unsigned)vreg_id;
 	unsigned _level		= (unsigned)level_mV;
-	int	 rc;
 
-	rc = msm_proc_comm(PCOM_VREG_SET_LEVEL, &_id, &_level);
-
-	if (rc)
-		return rc;
+	ret = msm_proc_comm(PCOM_VREG_SET_LEVEL, &_id, &_level);
+	if (ret)
+		return ret;
 
 	return _pcom_err_to_linux_errno(_id);
 }
@@ -80,299 +84,209 @@ static int _vreg_pull_down(int vreg_id, bool pull_down)
 	return msm_proc_comm(PCOM_VREG_PULLDOWN, &_enable, &_id);
 }
 
-struct proccomm_regulator_drvdata {
+struct proccomm_regulator_data {
 	struct regulator_desc	rdesc;
 	int			rise_time;
+	bool			pulldown;
+	bool			negative;
+
 	int			last_voltage;
 	bool			enabled;
-	bool			negative;
 };
 
 static int proccomm_vreg_enable(struct regulator_dev *rdev)
 {
-	struct proccomm_regulator_drvdata *ddata;
-	int rc;
+	int ret;
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
 
-	ddata = rdev_get_drvdata(rdev);
-	rc = _vreg_switch(rdev_get_id(rdev), VREG_SWITCH_ENABLE);
-
-	if (rc) {
+	ret = _vreg_switch(rdev_get_id(rdev), VREG_SWITCH_ENABLE);
+	if (ret) {
 		dev_err(rdev_get_dev(rdev),
-			"could not enable regulator %d (%s): %d\n",
-			rdev_get_id(rdev), ddata->rdesc.name, rc);
+			"failed to enable regulator %d (%s): %d\n",
+			rdev_get_id(rdev), data->rdesc.name, ret);
 	} else {
-		dev_dbg(rdev_get_dev(rdev),
-			"enabled regulator %d (%s)\n",
-			rdev_get_id(rdev), ddata->rdesc.name);
-		ddata->enabled = 1;
+		dev_dbg(rdev_get_dev(rdev), "enabled regulator %d (%s)\n",
+			rdev_get_id(rdev), data->rdesc.name);
+		data->enabled = 1;
 	}
 
-	return rc;
+	return ret;
 }
 
 static int proccomm_vreg_disable(struct regulator_dev *rdev)
 {
-	struct proccomm_regulator_drvdata *ddata;
-	int rc;
+	int ret;
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
 
-	ddata = rdev_get_drvdata(rdev);
-	rc = _vreg_switch(rdev_get_id(rdev), VREG_SWITCH_DISABLE);
-
-	if (rc) {
+	ret = _vreg_switch(rdev_get_id(rdev), VREG_SWITCH_DISABLE);
+	if (ret) {
 		dev_err(rdev_get_dev(rdev),
-			"could not disable regulator %d (%s): %d\n",
-			rdev_get_id(rdev), ddata->rdesc.name, rc);
+			"failed to disable regulator %d (%s): %d\n",
+			rdev_get_id(rdev), data->rdesc.name, ret);
 	} else {
-		dev_dbg(rdev_get_dev(rdev),
-			"disabled regulator %d (%s)\n",
-			rdev_get_id(rdev), ddata->rdesc.name);
-		ddata->enabled = 0;
+		dev_dbg(rdev_get_dev(rdev), "disabled regulator %d (%s)\n",
+			rdev_get_id(rdev), data->rdesc.name);
+		data->enabled = 0;
 	}
 
-	return rc;
+	return ret;
 }
 
 static int proccomm_vreg_is_enabled(struct regulator_dev *rdev)
 {
-	struct proccomm_regulator_drvdata *ddata = rdev_get_drvdata(rdev);
-
-	return ddata->enabled;
-}
-
-static int proccomm_vreg_rise_time(struct regulator_dev *rdev)
-{
-	struct proccomm_regulator_drvdata *ddata = rdev_get_drvdata(rdev);
-
-	return ddata->rise_time;
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
+	return data->enabled;
 }
 
 static int proccomm_vreg_get_voltage(struct regulator_dev *rdev)
 {
-
-	struct proccomm_regulator_drvdata *ddata = rdev_get_drvdata(rdev);
-
-	return MV_TO_UV(ddata->last_voltage);
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
+	return MV_TO_UV(data->last_voltage);
 }
 
 static int proccomm_vreg_set_voltage(struct regulator_dev *rdev,
-					int min_uV, int max_uV, unsigned *sel)
+				     int min_uV, int max_uV, unsigned *sel)
 {
-	struct proccomm_regulator_drvdata *ddata = rdev_get_drvdata(rdev);
+	int ret;
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
 	int level_mV = UV_TO_MV(min_uV);
-	int rc;
 
-	rc = _vreg_set_level(rdev_get_id(rdev),
-			ddata->negative ? -level_mV : level_mV);
-
-	if (rc) {
+	ret = _vreg_set_level(rdev_get_id(rdev),
+			      data->negative ? -level_mV : level_mV);
+	if (ret) {
 		dev_err(rdev_get_dev(rdev),
-			"could not set voltage for regulator %d (%s) "
+			"failed to set voltage for regulator %d (%s) "
 			"to %d mV: %d\n",
-			rdev_get_id(rdev), ddata->rdesc.name, level_mV, rc);
+			rdev_get_id(rdev), data->rdesc.name, level_mV, ret);
 	} else {
 		dev_dbg(rdev_get_dev(rdev),
 			"voltage for regulator %d (%s) set to %d mV\n",
-			rdev_get_id(rdev), ddata->rdesc.name, level_mV);
-		ddata->last_voltage = level_mV;
+			rdev_get_id(rdev), data->rdesc.name, level_mV);
+		data->last_voltage = level_mV;
 	}
 
-	return rc;
+	return ret;
 }
 
-static struct regulator_ops proccomm_regulator_ops = {
+static struct regulator_ops proccomm_vreg_ops = {
 	.enable		= proccomm_vreg_enable,
 	.disable	= proccomm_vreg_disable,
 	.is_enabled	= proccomm_vreg_is_enabled,
 	.get_voltage	= proccomm_vreg_get_voltage,
 	.set_voltage	= proccomm_vreg_set_voltage,
-	.enable_time	= proccomm_vreg_rise_time,
 };
-
-/*
- * Create and register a struct regulator_dev based on the information in
- * a struct proccomm_regulator_info.
- * Fills in the rdev field in struct proccomm_regulator_info.
- */
-static struct regulator_dev *create_proccomm_rdev(
-	struct proccomm_regulator_info *info, struct device *parent)
-{
-	const char *name;
-	struct proccomm_regulator_drvdata *d;
-	struct regulator_dev *rdev;
-	struct regulator_config config = { };
-	int rc = 0;
-
-	if (info->id < 0) {
-		dev_err(parent, "invalid regulator id %d\n", info->id);
-		rc = -EINVAL;
-		goto out;
-	}
-
-	name = info->init_data.constraints.name;
-
-	if (!name) {
-		dev_err(parent,
-			"could not register regulator with id %d: "
-			"no name specified\n", info->id);
-		rc = -EINVAL;
-		goto out;
-	}
-
-	if (info->pulldown > 0) {
-		rc = _vreg_pull_down(info->id, info->pulldown);
-		if (rc) {
-			dev_err(parent,
-				"probing for regulator %d (%s) failed\n",
-				info->id, name);
-			goto out;
-		}
-	}
-
-	d = kzalloc(sizeof(*d), GFP_KERNEL);
-
-	if (!d) {
-		dev_err(parent,
-			"could not allocate struct proccomm_regulator_drvdata "
-			"for regulator %d (%s)\n", info->id, name);
-		rc = -ENOMEM;
-		goto out;
-	}
-
-	d->rdesc.name	= name;
-	d->rdesc.id	= info->id;
-	d->rdesc.ops	= &proccomm_regulator_ops;
-	d->rdesc.type	= REGULATOR_VOLTAGE;
-	d->rdesc.owner	= THIS_MODULE;
-	d->rise_time	= info->rise_time;
-	d->enabled	= 0;
-	d->negative	= info->negative;
-	d->rdesc.n_voltages = info->n_voltages;
-
-	config.dev = parent;
-	config.init_data = &info->init_data;
-	config.driver_data = d;
-
-	rdev = regulator_register(&d->rdesc, &config);
-
-	if (IS_ERR(rdev)) {
-		rc = PTR_ERR(rdev);
-		dev_err(parent, "error registering regulator %d (%s): %d\n",
-				info->id, name, rc);
-		goto clean;
-	}
-
-	dev_dbg(parent, "registered regulator %d (%s)\n", info->id, name);
-
-	return rdev;
-
-clean:
-	kfree(d);
-out:
-	return ERR_PTR(rc);
-}
-
-/*
- * Unregister and destroy a struct regulator_dev created by
- * create_proccomm_rdev.
- */
-static void destroy_proccomm_rdev(struct regulator_dev *rdev)
-{
-	struct proccomm_regulator_drvdata *d;
-
-	if (!rdev)
-		return;
-
-	d = rdev_get_drvdata(rdev);
-
-	regulator_unregister(rdev);
-
-	dev_dbg(rdev_get_dev(rdev)->parent,
-		"unregistered regulator %d (%s)\n",
-		d->rdesc.id, d->rdesc.name);
-
-	kfree(d);
-}
-
 
 static int proccomm_vreg_probe(struct platform_device *pdev)
 {
+	int ret;
 	struct device *dev = &pdev->dev;
-	struct proccomm_regulator_platform_data *pdata = dev->platform_data;
-	struct regulator_dev **rdevs;
-	int rc = 0;
-	size_t i = 0;
+	struct device_node *node = dev->of_node;
+	struct proccomm_regulator_data *data;
+	uint32_t val;
 
-	if (!pdata) {
-		dev_err(dev, "invalid platform data\n");
-		rc = -EINVAL;
-		goto check_fail;
+	struct regulator_init_data *initdata;
+	struct regulator_desc *rdesc;
+	struct regulator_dev *rdev;
+	struct regulator_config config = { };
+
+	if (!node) {
+		dev_err(dev, "no device tree data supplied\n");
+		return -EINVAL;
 	}
 
-	if (pdata->nregs == 0) {
-		dev_err(dev, "registering an empty regulator list; "
-				"this is probably not what you want\n");
-		rc = -EINVAL;
-		goto check_fail;
+	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
+	if (!data) {
+		dev_err(dev, "failed to allocate memory\n");
+		return -ENOMEM;
 	}
 
-	rdevs = kcalloc(pdata->nregs, sizeof(*rdevs), GFP_KERNEL);
+	rdesc = &data->rdesc;
 
-	if (!rdevs) {
-		dev_err(dev, "could not allocate storage for "
-				"struct regulator_dev array\n");
-		rc = -ENOMEM;
-		goto check_fail;
+	ret = of_property_read_u32(node, "id", &val);
+	if (ret) {
+		dev_err(dev, "failed to read 'id' ret=%d\n", ret);
+		return ret;
 	}
+	rdesc->id = val;
 
-	platform_set_drvdata(pdev, rdevs);
+	ret = of_property_read_u32(node, "rise-time", &val);
+	if (ret) {
+		dev_err(dev, "failed to read 'rise-time' ret=%d\n", ret);
+		return ret;
+	}
+	rdesc->enable_time = val;
 
-	dev_dbg(dev, "registering %d proccomm regulators\n", pdata->nregs);
+	ret = of_property_read_u32(node, "pulldown", &val);
+	if (ret) {
+		dev_err(dev, "failed to read 'pulldown' ret=%d\n", ret);
+		return ret;
+	}
+	data->pulldown = !!val;
 
-	for (i = 0; i < pdata->nregs; i++) {
-		rdevs[i] = create_proccomm_rdev(&pdata->regs[i], dev);
-		if (IS_ERR(rdevs[i])) {
-			rc = PTR_ERR(rdevs[i]);
-			goto backout;
+	ret = of_property_read_u32(node, "negative", &val);
+	if (ret) {
+		dev_err(dev, "failed to read 'negative' ret=%d\n", ret);
+		return ret;
+	}
+	data->negative = !!val;
+
+	if (data->pulldown) {
+		ret = _vreg_pull_down(rdesc->id, data->pulldown);
+		if (ret) {
+			dev_err(dev, "failed to pull down regulator ret=%d\n",
+				ret);
+			return ret;
 		}
 	}
 
-	dev_dbg(dev, "%d proccomm regulators registered\n", pdata->nregs);
+	initdata = of_get_regulator_init_data(dev, node);
+	if (!initdata) {
+		dev_err(dev, "failed to get regulator init data\n");
+		return -ENOMEM;
+	}
 
-	return rc;
+	rdesc->name = initdata->constraints.name;
+	rdesc->ops = &proccomm_vreg_ops;
+	rdesc->type = REGULATOR_VOLTAGE;
+	rdesc->owner = THIS_MODULE;
 
-backout:
-	while (--i >= 0)
-		destroy_proccomm_rdev(rdevs[i]);
+	config.dev = dev;
+	config.init_data = initdata;
+	config.driver_data = data;
+	config.of_node = node;
 
-	kfree(rdevs);
+	rdev = regulator_register(rdesc, &config);
+	if (IS_ERR(rdev)) {
+		ret = PTR_ERR(rdev);
+		dev_err(dev, "failed to register regulator ret=%d\n", ret);
+		return ret;
+	}
 
-check_fail:
-	return rc;
-}
-
-static int proccomm_vreg_remove(struct platform_device *pdev)
-{
-	struct proccomm_regulator_platform_data *pdata;
-	struct regulator_dev **rdevs;
-	size_t i;
-
-	pdata = pdev->dev.platform_data;
-	rdevs = platform_get_drvdata(pdev);
-
-	for (i = 0; i < pdata->nregs; i++)
-		destroy_proccomm_rdev(rdevs[i]);
-
-	kfree(rdevs);
+	platform_set_drvdata(pdev, rdev);
 
 	return 0;
 }
 
+static int proccomm_vreg_remove(struct platform_device *pdev)
+{
+	struct regulator_dev *rdev = platform_get_drvdata(pdev);
+	regulator_unregister(rdev);
+	return 0;
+}
+
+static struct of_device_id proccomm_vreg_of_match_table[] = {
+	{ .compatible = "qcom,proccomm-regulator", },
+	{ /* end of table */ }
+};
+MODULE_DEVICE_TABLE(of, proccomm_vreg_of_match_table);
+
 static struct platform_driver proccomm_vreg_driver = {
 	.probe	= proccomm_vreg_probe,
-	.remove = proccomm_vreg_remove,
-	.driver = {
-		.name	= PROCCOMM_REGULATOR_DEV_NAME,
-		.owner	= THIS_MODULE,
+	.remove	= proccomm_vreg_remove,
+	.driver	= {
+		.name		= PROCCOMM_REGULATOR_DEV_NAME,
+		.owner		= THIS_MODULE,
+		.of_match_table	= proccomm_vreg_of_match_table,
 	},
 };
 
